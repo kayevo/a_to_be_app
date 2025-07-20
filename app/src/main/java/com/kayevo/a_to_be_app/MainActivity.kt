3c14cd2059ee138c95fc00e7e1988a0fcb435593
Johantan Kayevo <kayevo@protonmail.com>
2025-07-20 10:45:28 +0100

    feature: add home screen.

--- app/src/main/java/com/kayevo/a_to_be_app/MainActivity.kt
+++ app/src/main/java/com/kayevo/a_to_be_app/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.kayevo.a_to_be_app.ui.home.HomeScreen
+import com.kayevo.a_to_be_app.ui.navigation.Screen
 import com.kayevo.a_to_be_app.ui.theme.A_to_be_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +27,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             A_to_be_AppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(
+                                WindowInsets.statusBars
+                                    .only(WindowInsetsSides.Top)
+                                    .asPaddingValues()
+                            )
+                            .padding(bottom = 32.dp)
+                            .padding(top = 16.dp),
+                    ) {
+                        val navController = rememberNavController()
+
+                        NavHost(navController = navController, startDestination = Screen.HOME.toString()) {
+                            composable(Screen.HOME.toString()) { HomeScreen(navController) }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    A_to_be_AppTheme {
-        Greeting("Android")
-    }
-}
