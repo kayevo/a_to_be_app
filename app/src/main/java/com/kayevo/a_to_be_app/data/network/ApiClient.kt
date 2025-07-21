56402a123250b1fc093e165cb0115379f2eea873
Johantan Kayevo <kayevo@protonmail.com>
2025-07-21 20:55:24 +0100

    feature: add API Client.

---
+++ app/src/main/java/com/kayevo/a_to_be_app/data/network/ApiClient.kt
@@ -0,0 +1,25 @@
+package com.kayevo.a_to_be_app.data.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+object ApiClient {
+
+    private const val BASE_URL = "https://dummyjson.com/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    val builder: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient)
+        .build()
+}
